5eaa1c50eca946102c602897c3605d1c040eb053
deminy717 <ny.demi.717@gmail.com>
2021-03-15 18:43:26 -0400

    modified localization, target_identifier, processor

--- src/processor/scripts/processor.py
+++ src/processor/scripts/processor.py
@@ -11,6 +11,7 @@ from geometry_msgs.msg import Twist, TransformStamped
 from sensor_msgs.msg import Image
 
 from cv_bridge import CvBridge
+import numpy as np
 
 class Processor():
     def __init__(self):
@@ -20,13 +21,23 @@ class Processor():
         self.sub_pos = rospy.Subscriber("/lab3_pos", TransformStamped, self.store_pos)
         self.pub_locations = rospy.Publisher('/lab3_locations', numpy_msg(Floats), queue_size="0")
         self.bridge = CvBridge()
+        self.locations = []
         return
 
     def identify_targets(self, msg):
         img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
         pixels = self.target_identifier.identify_targets(img)
-        locations = self.localizer.localize(pixels, self.pos)
-        self.pub_locations.publish(locations.flatten())
+        if not pixels:
+            return
+        else:
+            self.locations.append(self.localizer.localize(pixels, self.pos))
+            self.pub_locations.publish(self.locations[-1])
+
+            print self.locations[-1]
+            f = open('/home/demi/aer1217/labs/src/processor/location.txt', 'a')
+            f.writelines('\n' + np.array2string(self.locations[-1][0])+' '+ np.array2string(self.locations[-1][1]))
+            f.close()
+
         return
 
     def store_pos(self, msg):
--- src/processor/scripts/target_identifier.py
+++ src/processor/scripts/target_identifier.py
@@ -1,10 +1,72 @@
+import roslib
+import rospy
+import numpy as np
+import cv2
 
+from geometry_msgs.msg import TransformStamped, Twist
+from tf.transformations import euler_from_quaternion, quaternion_from_euler, quaternion_matrix
 
 class TargetIdentifier():
     def __init__(self):
         self.pixels = []
+
+        self.k_matrix = np.array([[698.86, 0, 306.91],
+                                  [0, 699.13, 150.34],
+                                  [0, 0, 1]])
+        self.k_inv = np.linalg.inv(self.k_matrix)
+
+        self.d = np.array([0.191887, -0.563680, -0.003676, -0.002037, 0])
+
         return
 
-    def identify_targets(self, img):
+    def img_undist(self,img):
 
-        return self.pixels
+        img_undistort = cv2.undistort(img, self.k_matrix, self.d, None)
+
+        return img_undistort
+
+    def identify_targets(self, img_original):
+        img = self.img_undist(img_original)
+
+        img2 = cv2.inRange(img, (140, 140, 140), (160, 160, 160))
+
+        _,contours,_ = cv2.findContours(img2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        contour_list = []
+        center_list = []
+        for contour in contours:
+            approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
+            area = cv2.contourArea(contour)
+            if ((len(approx) > 8) & (area > 30)):
+                contour_list.append(contour)
+                M = cv2.moments(contour)
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+                center_list.append([cX, cY])
+
+
+
+        # img2 = cv2.inRange(img, (140, 140, 140), (160, 160, 160))
+        #
+        # _, contours, _ = cv2.findContours(binary_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        #
+        # center_list = []
+        # rad_list = []
+        # contour_list = []
+        # dist_thresh = 2
+        # rad_thresh = 10
+        # for contour in contours:
+        #     (x, y), radius = cv2.minEnclosingCircle(contour)
+        #     sum_diff = 0
+        #     for con in contour:
+        #         dist = np.sqrt((con[0][0] - x) ** 2 + (con[0][1] - y) ** 2)
+        #         diff = abs(dist - radius)
+        #         sum_diff += diff
+        #     sum_diff /= len(contour)
+        #     # print(sum_diff)
+        #     if sum_diff < dist_thresh and radius > rad_thresh:
+        #         center_list.append((int(x), int(y)))
+        #         rad_list.append(radius)
+        #         contour_list.append(contour)
+
+        return center_list
